14332f9718382fefdcdc9259d44bd4a09a69bb0d
yiru <yiru@student.unsw.edu.au>
2025-07-11 01:25:10 +1000

    Loaded model and added orbitControls

--- room-pf/src/pages/Home.js
+++ room-pf/src/pages/Home.js
@@ -1,13 +1,16 @@
 import * as THREE from 'three';
 import { loadRoomModel } from '../components/room.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 export class HomePage {
     constructor() {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+        
 
-        this.renderer = new THREE.WebGLRenderer();
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement);
         document.body.appendChild( this.renderer.domElement );
     }
 
@@ -19,7 +22,24 @@ export class HomePage {
         const room = await loadRoomModel();
         this.scene.add(room);
 
-        this.camera.position.z = 5;
+        const color = 0xFFFFFF
+        const intensity = 3;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        this.scene.add(light);
+
+        this.camera.position.set(0, 2, 25);
+        this.controls.update();
+        room.rotation.set(0.4, 0.8, 0);
+        room.position.y = -4
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            this.controls.update();
+            this.renderScene();
+        };
+
+        animate();
     }
 
 }
