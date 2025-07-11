84af3d7f7aaa4fcf3f514aba51868e2fd6311b2b
yiru jang <z5480998@ad.unsw.edu.au>
2025-07-12 00:24:15 +1000

    IMplemented texture

--- room-pf/src/components/room.js
+++ room-pf/src/components/room.js
@@ -1,5 +1,9 @@
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { TextureLoader } from 'three';
+import { MeshBasicMaterial } from 'three';
+
+const textureLoader = new TextureLoader();
 
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/draco/');
@@ -8,10 +12,39 @@ const loader = new GLTFLoader();
 loader.setDRACOLoader(dracoLoader);
 
 export function loadRoomModel() {
+    const textures = {
+        textureOne: textureLoader.load('/textures/FirstTextureSet.webp'),
+        textureTwo: textureLoader.load('/textures/SecondTextureSet.webp'),
+        textureThree: textureLoader.load('/textures/ThirdTextureSet.webp'),
+        textureFour: textureLoader.load('/textures/FourthTextureSet.webp')
+    }
+
     return new Promise((resolve, reject) => {
         loader.load(
             "/models/room_portfolio_compressed.glb", 
-            (gltf) => resolve(gltf.scene),
+            (gltf) => {
+                const model = gltf.scene;
+
+                Object.values(textures).forEach((texture) => {
+                    texture.flipY = false;
+                });
+
+                model.traverse((child) => {
+                    if (child.isMesh) {
+                        if (child.name.toLowerCase().includes("first")) {
+                            child.material = new MeshBasicMaterial({ map: textures.textureOne});
+                        } else if (child.name.toLowerCase().includes("second")) {
+                            child.material = new MeshBasicMaterial({ map: textures.textureTwo});
+                        } else if (child.name.toLowerCase().includes("third")) {
+                            child.material = new MeshBasicMaterial({ map: textures.textureThree});
+                        } else {
+                            child.material = new MeshBasicMaterial({ map: textures.textureFour});
+                        }
+                    }
+                })
+
+                resolve(model);
+            },
             undefined,
             (error) => reject(error)
         );
--- room-pf/src/pages/Home.js
+++ room-pf/src/pages/Home.js
@@ -5,12 +5,18 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 export class HomePage {
     constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-        
+        this.scene.background = new THREE.Color('#f6f1eb');  
+
+        this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100);
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+
         this.controls = new OrbitControls( this.camera, this.renderer.domElement);
+        this.controls.enableDamping = true;
+        this.controls.zoom = 0.2;
+
         document.body.appendChild( this.renderer.domElement );
     }
 
@@ -21,17 +27,33 @@ export class HomePage {
     async init() {
         const room = await loadRoomModel();
         this.scene.add(room);
-
+        
         const color = 0xFFFFFF
         const intensity = 3;
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
         this.scene.add(light);
 
-        this.camera.position.set(0, 2, 25);
-        this.controls.update();
-        room.rotation.set(0.4, 0.8, 0);
-        room.position.y = -4
+        this.camera.position.set(0, 2, 65);
+
+        room.rotation.set(0.3, 0.8, 0);
+        room.position.y = -5
+        room.position.x = 0;
+
+        // rotation limits
+        this.controls.maxPolarAngle = Math.PI / 1.75;
+        this.controls.minPolarAngle = Math.PI / 2;
+
+        this.controls.minAzimuthAngle = - Math.PI / 6;
+        this.controls.maxAzimuthAngle = Math.PI / 6;
+
+        // preserve the original update settings into originalUpdate
+        const originalUpdate = this.controls.update.bind(this.controls);
+        
+        // Distance to limit edges
+        window.addEventListener('mousemove', (event) => {
+            mouseX = event.clientX;
+        })
 
         const animate = () => {
             requestAnimationFrame(animate);
